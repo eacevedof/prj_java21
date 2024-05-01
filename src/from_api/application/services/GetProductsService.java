3e846bdca922928c7c75268077b5e7114f37aefe
ioedumac <eacevedof@yahoo.es>
2024-05-01 23:34:04 +0200

    mmm get result generico

--- src/from_api/application/services/GetProductsService.java
+++ src/from_api/application/services/GetProductsService.java
@@ -11,20 +11,13 @@ import java.util.List;
 
 public final class GetProductsService {
 
-    private final String _productsUrl = "https://json.theframework.es/index.php?getfile=app_product.json";
 
     public void invoke() throws Exception {
         try {
-            String jsonProducts = FileGetContent.getInstance().getContentFromUrl(this._productsUrl);
-            Type reflectProductList = new TypeToken<List<ProductEntity>>(){}.getType();
-            List<ProductEntity> productEntityList = Json.getInstance().getJsonStringAsEntity(jsonProducts, reflectProductList);
-            for (ProductEntity product : productEntityList) {
-                System.out.println(product);
-            }
-            Log.console(jsonProducts);
+
         }
         catch (Exception e) {
-            GetProductsException.ErrorOnReadingEndpoint(this._productsUrl);
+
         }
     }
 }
