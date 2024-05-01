7c181aa936c4a20c78f414048615f8fdf2a7681f
ioedumac <eacevedof@yahoo.es>
2024-05-02 00:30:17 +0200

    get-result generico ok

--- src/shared/infrastructure/repositories/AbstractApiRepository.java
+++ src/shared/infrastructure/repositories/AbstractApiRepository.java
@@ -1,11 +1,9 @@
 package shared.infrastructure.repositories;
 
-import com.google.gson.reflect.TypeToken;
-import from_api.domain.entities.ProductEntity;
 import shared.domain.entities.AbstractEntity;
 import shared.infrastructure.file.FileGetContent;
 import shared.infrastructure.formatters.Json;
-import java.lang.reflect.Type;
+
 import java.util.List;
 
 public abstract class AbstractApiRepository {
@@ -13,7 +11,7 @@ public abstract class AbstractApiRepository {
     protected <T extends AbstractEntity> List<T> getResult(String endpoint, Class<T> entityClass) {
         try {
             String jsonProducts = FileGetContent.getInstance().getContentFromUrl(endpoint);
-            return Json.getInstance().getJsonStringAsEntity(jsonProducts, entityClass);
+            return Json.getInstance().getJsonStringAsList(jsonProducts, entityClass);
         } catch (Exception e) {
             e.printStackTrace();
             return null;
