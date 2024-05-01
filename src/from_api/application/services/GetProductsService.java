f10e04a5d60e6d7cc9f0976c7138199c5b9d38ec
ioedumac <eacevedof@yahoo.es>
2024-05-02 00:04:32 +0200

    get-result generico ok

--- src/from_api/application/services/GetProductsService.java
+++ src/from_api/application/services/GetProductsService.java
@@ -2,15 +2,16 @@ package from_api.application.services;
 
 
 import from_api.domain.entities.ProductEntity;
+import from_api.domain.repositories.InterfaceProductsRepository;
 import from_api.infrastructure.repositories.ProductsRepository;
 import shared.infrastructure.io.Log;
 
 public final class GetProductsService {
 
-
     public void invoke() throws Exception {
         try {
-            var products = ProductsRepository.getInstance().getAllProducts();
+            InterfaceProductsRepository productsRepository = ProductsRepository.getInstance();
+            var products = productsRepository.getAllProducts();
             for (ProductEntity product : products) {
                 Log.console(product.toString());
             }
