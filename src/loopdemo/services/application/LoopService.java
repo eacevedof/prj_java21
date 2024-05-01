36966ba6c6eae4cd849e5f3edb53d067dfb032dc
ioedumac <eacevedof@yahoo.es>
2024-05-01 17:40:46 +0200

    moving to service

--- src/loopdemo/services/application/LoopService.java
+++ src/loopdemo/services/application/LoopService.java
@@ -2,10 +2,13 @@ package loopdemo.services.application;
 
 import shared.infrastructure.io.Log;
 
-final class LoopService {
+public final class LoopService {
     // Readonly property (not directly available in PHP, but could be achieved through getters only)
     //private final String readonlyProperty;
     public void Invoke() {
-        Log.Info("start loop service");
+        Log.InfoNl("LoopService");
+        for (int i = 1; i <= 5; i++) {
+            Log.InfoNl("i = " + i);
+        }
     }
 }
