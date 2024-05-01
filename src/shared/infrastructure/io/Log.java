62d822eac00c079552ce0d9d6031d1a0d95a2ffc
ioedumac <eacevedof@yahoo.es>
2024-05-01 17:59:43 +0200

    loop controller

--- src/shared/infrastructure/io/Log.java
+++ src/shared/infrastructure/io/Log.java
@@ -1,12 +1,6 @@
 package shared.infrastructure.io;
 
 public final class Log {
-
-    public static void Info(String message)
-    {
-        System.out.print(message);
-    }
-
     public static void InfoNl(String message)
     {
         System.out.println(message);
