4b71e908223cbe9d1df52b2f4c6b28dd7fbfd3eb
ioedumac <eacevedof@yahoo.es>
2024-05-01 21:13:39 +0200

    json parser

--- src/shared/infrastructure/formatters/Json.java
+++ src/shared/infrastructure/formatters/Json.java
@@ -3,7 +3,13 @@ package shared.infrastructure.formatters;
 import com.google.gson.Gson;
 
 public final class Json {
-    public Object getJsonStringAsObject(String json, String classUrl) {
 
+    public static Json getInstance() {
+        return (new Json());
+    }
+
+    public Object getJsonStringAsObject(String jsonString, Class<?> anyClass) {
+        Gson gson = new Gson();
+        return gson.fromJson(jsonString, anyClass);
     }
 }
