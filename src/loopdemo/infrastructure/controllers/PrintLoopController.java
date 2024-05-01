239ffd9fc43862994fee6eb0ebef169353bce155
ioedumac <eacevedof@yahoo.es>
2024-05-01 17:49:57 +0200

    loop controller

--- src/loopdemo/infrastructure/controllers/PrintLoopController.java
+++ src/loopdemo/infrastructure/controllers/PrintLoopController.java
@@ -1,9 +1,13 @@
 package loopdemo.infrastructure.controllers;
 
+import shared.infrastructure.io.Log;
+import loopdemo.application.services.LoopService;
+
 public final class PrintLoopController {
 
     public void Invoke()
     {
-
+        Log.InfoNl("PrintController");
+        (new LoopService()).Invoke();
     }
 }
