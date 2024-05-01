bcc2d5791bf8beca7fab597e23b1ad42ab8c5b42
ioedumac <eacevedof@yahoo.es>
2024-05-01 18:40:14 +0200

    json-reader

--- src/json/application/services/GetProductsService.java
+++ src/json/application/services/GetProductsService.java
@@ -1,14 +1,14 @@
 package json.application.services;
 
 import shared.infrastructure.io.Log;
+import shared.infrastructure.file.FileGetContent;
 
 public final class GetProductsService {
     // Readonly property (not directly available in PHP, but could be achieved through getters only)
-    //private final String readonlyProperty;
+    private final String readonlyProperty = "https://json.theframework.es/index.php?getfile=app_product.json";
+
     public void Invoke() {
-        Log.InfoNl("LoopService");
-        for (int i = 1; i <= 5; i++) {
-            Log.InfoNl("i = " + i);
-        }
+        String json = FileGetContent.getInstance().GetContentFromUrlOrFail(this.readonlyProperty);
+        Log.InfoNl(json);
     }
 }
