4e8ebb36a21cb73c9d2f7b8b89357c84245a125d
ioedumac <eacevedof@yahoo.es>
2024-05-01 17:49:57 +0200

    loop controller

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 import shared.infrastructure.io.Log;
 
-import loopdemo.application.services.LoopService;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import loopdemo.infrastructure.controllers.PrintLoopController;
+
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         Log.InfoNl("** Main.main ***");
-        (new LoopService()).Invoke();
+        (new PrintLoopController()).Invoke();
     }
 }
