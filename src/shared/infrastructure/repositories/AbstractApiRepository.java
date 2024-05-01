bb3561d05bba628800da51680b40fb2cdda72df3
ioedumac <eacevedof@yahoo.es>
2024-05-01 22:32:49 +0200

    no serealiza de modo generico

--- src/shared/infrastructure/repositories/AbstractApiRepository.java
+++ src/shared/infrastructure/repositories/AbstractApiRepository.java
@@ -1,5 +1,25 @@
 package shared.infrastructure.repositories;
 
+import com.google.gson.reflect.TypeToken;
+import from_api.domain.entities.ProductEntity;
+import shared.domain.entities.AbstractEntity;
+import shared.infrastructure.file.FileGetContent;
+import shared.infrastructure.formatters.Json;
+import java.lang.reflect.Type;
+import java.util.List;
+
 public abstract class AbstractApiRepository {
+
+    protected List<AbstractEntity> getResult(String endpoint) {
+        try {
+            String jsonProducts = FileGetContent.getInstance().getContentFromUrl(endpoint);
+
+            return Json.getInstance().getJsonStringAsEntity(jsonProducts, ProductEntity.class);
+
+        }
+        catch (Exception e) {
+            return null;
+        }
+    }
 
 }
