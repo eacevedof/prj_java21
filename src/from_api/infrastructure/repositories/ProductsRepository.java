5458341f14ef8d9d6f0f7e72867a1e0e85e98c6d
ioedumac <eacevedof@yahoo.es>
2024-05-01 23:46:20 +0200

    mmm get result generico

--- src/from_api/infrastructure/repositories/ProductsRepository.java
+++ src/from_api/infrastructure/repositories/ProductsRepository.java
@@ -2,10 +2,13 @@ package from_api.infrastructure.repositories;
 
 import from_api.domain.entities.ProductEntity;
 import from_api.domain.repositories.InterfaceProductsRepository;
+import shared.infrastructure.repositories.AbstractApiRepository;
 
 import java.util.List;
 
-public final class ProductsRepository implements InterfaceProductsRepository {
+public final class ProductsRepository extends AbstractApiRepository implements InterfaceProductsRepository {
+
+    private static final String PRODUCTS_ENDPOINT = "https://json.theframework.es/index.php?getfile=app_product.json";
 
     public static ProductsRepository getInstance() {
         return new ProductsRepository();
@@ -13,6 +16,6 @@ public final class ProductsRepository implements InterfaceProductsRepository {
 
     @Override
     public List<ProductEntity> getAllProducts() {
-        return List.of();
+        return this.getResult(PRODUCTS_ENDPOINT, ProductEntity.class);
     }
 }
