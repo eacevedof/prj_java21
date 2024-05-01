cea25cab78804fefb740d80b4f46cf8fe4d19e60
ioedumac <eacevedof@yahoo.es>
2024-05-01 19:45:32 +0200

    exceptions

--- src/json/application/services/GetProductsService.java
+++ src/json/application/services/GetProductsService.java
@@ -2,13 +2,19 @@ package json.application.services;
 
 import shared.infrastructure.io.Log;
 import shared.infrastructure.file.FileGetContent;
+import json.application.exceptions.GetProductsException;
 
 public final class GetProductsService {
-    // Readonly property (not directly available in PHP, but could be achieved through getters only)
-    private final String readonlyProperty = "https://json.theframework.es/index.php?getfile=app_product.json";
+
+    private final String _productsUrl = "https://json.theframework.es/index.php?getfile=app_product.json";
 
     public void Invoke() {
-        String json = FileGetContent.getInstance().GetContentFromUrlOrFail(this.readonlyProperty);
-        Log.InfoNl(json);
+        try {
+            String json = FileGetContent.getInstance().GetContentFromUrlOrFail(this._productsUrl);
+            Log.InfoNl(json);
+        }
+        catch (Exception e) {
+            GetProductsException.ErrorOnReadingEndpoint(this._productsUrl);
+        }
     }
 }
