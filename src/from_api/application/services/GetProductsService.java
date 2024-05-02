b344d0910e6370a3146ff9dab9032848140cf178
ioedumac <eacevedof@yahoo.es>
2024-05-02 12:51:28 +0200

    from primitives

--- src/from_api/application/services/GetProductsService.java
+++ src/from_api/application/services/GetProductsService.java
@@ -4,15 +4,19 @@ import from_api.domain.entities.ProductEntity;
 import from_api.domain.repositories.InterfaceProductsRepository;
 import from_api.infrastructure.repositories.ProductsRepository;
 import shared.infrastructure.io.Log;
+import java.util.ArrayList;
+import java.util.List;
 
 public final class GetProductsService {
 
     public void invoke() throws Exception {
+        List<String> products = new ArrayList<>();
         try {
             InterfaceProductsRepository productsRepository = ProductsRepository.getInstance();
-            var products = productsRepository.getAllProducts();
-            for (ProductEntity product : products) {
-                Log.console(product.toString());
+            var productsEntities = productsRepository.getAllProducts();
+            for (ProductEntity productEntity : productsEntities) {
+
+                Log.console(productEntity.toString());
             }
         }
         catch (Exception e) {
