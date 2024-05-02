c58d8c1e109eddd724e6f6ccc98889815abd546b
ioedumac <eacevedof@yahoo.es>
2024-05-02 10:05:12 +0200

    requesters

--- src/shared/infrastructure/http/requests/GETRequest.java
+++ src/shared/infrastructure/http/requests/GETRequest.java
@@ -2,23 +2,35 @@ package shared.infrastructure.http.requests;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.net.URI;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URI;
 
 public final class GETRequest {
+
+    private static final String METHOD_GET = "GET";
+    private int statusCode;
+
     public static GETRequest getInstance() {
         return new GETRequest();
     }
 
-    public String get(String urlEndpoint) throws Exception {
+    public String sendRequest(String urlEndpoint) throws Exception {
+        statusCode = -1;
         URL objUrl = URI.create(urlEndpoint).toURL();
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(objUrl.openStream()));
-        StringBuilder stringBuilder = new StringBuilder();
-        String strLine;
-        while ((strLine = bufferedReader.readLine()) != null) {
-            stringBuilder.append(strLine);
+        HttpURLConnection connection = (HttpURLConnection) objUrl.openConnection();
+        connection.setRequestMethod(METHOD_GET);
+        statusCode = connection.getResponseCode();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+        String lineRed;
+        StringBuilder responseAccumulator = new StringBuilder();
+        while ((lineRed = bufferedReader.readLine()) != null) {
+            responseAccumulator.append(lineRed);
         }
         bufferedReader.close();
-        return stringBuilder.toString();
+        return responseAccumulator.toString();
     }
+
+    public int getStatusCode() { return statusCode; }
 }
