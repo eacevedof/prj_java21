39d695c9db4c97e98b6803079f6fe48d2bab5f85
ioedumac <eacevedof@yahoo.es>
2024-05-02 13:09:53 +0200

    dtos ok

--- src/from_api/infrastructure/controllers/GetProductsController.java
+++ src/from_api/infrastructure/controllers/GetProductsController.java
@@ -1,13 +1,15 @@
 package from_api.infrastructure.controllers;
 
 import from_api.application.services.GetProductsService;
+import java.util.List;
+import from_api.application.dtos.GetProductDto;
 
 public final class GetProductsController {
 
     public void invoke()
     {
         try {
-            (new GetProductsService()).invoke();
+            var products = (new GetProductsService()).invoke();
         }
         catch (Exception e) {
             e.printStackTrace();
