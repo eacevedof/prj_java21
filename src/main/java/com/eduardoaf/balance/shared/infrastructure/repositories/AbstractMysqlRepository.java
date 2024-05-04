eeacc0e72ea8ef58b0389cd27720d63e75e5aed9
ioedumac <eacevedof@yahoo.es>
2024-05-05 01:28:48 +0200

    mysql repo

--- src/main/java/com/eduardoaf/balance/shared/infrastructure/repositories/AbstractMysqlRepository.java
+++ src/main/java/com/eduardoaf/balance/shared/infrastructure/repositories/AbstractMysqlRepository.java
@@ -3,15 +3,13 @@ package com.eduardoaf.balance.shared.infrastructure.repositories;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
-public abstract class AbstractMysqlRepository {
+public abstract class AbstractMysqlRepository<T> {
 
     private final JdbcTemplate jdbcTemplate;
 
-    public AbstractMysqlRepository(JdbcTemplate jdbcTemplate) {
+    protected AbstractMysqlRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
@@ -19,7 +17,7 @@ public abstract class AbstractMysqlRepository {
         jdbcTemplate.update(sql);
     }
 
-    protected List<T> getData(RowMapper<T> rowMapper, String sql) {
+    public List<T> getData(String sql, RowMapper<T> rowMapper) {
         return jdbcTemplate.query(sql, rowMapper);
     }
 }
