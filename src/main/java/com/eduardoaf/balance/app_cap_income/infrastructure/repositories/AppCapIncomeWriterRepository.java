960ab19ad4365b9b34ce0a437005f90252831ba7
ioedumac <eacevedof@yahoo.es>
2024-05-05 14:36:13 +0200

    abstract dto

---
+++ src/main/java/com/eduardoaf/balance/app_cap_income/infrastructure/repositories/AppCapIncomeWriterRepository.java
@@ -0,0 +1,14 @@
+package com.eduardoaf.balance.app_cap_income.infrastructure.repositories;
+
+import com.eduardoaf.balance.app_cap_income.domain.entities.ProductEntity;
+import com.eduardoaf.balance.app_cap_income.domain.repositories.InterfaceProductsRepository;
+import com.eduardoaf.balance.shared.infrastructure.repositories.AbstractApiRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public final class AppCapIncomeWriterRepository extends AbstractApiRepository {
+
+    public void insertIncome()
+}
--- src/main/java/com/eduardoaf/balance/shared/domain/entities/AbstractEntity.java
+++ src/main/java/com/eduardoaf/balance/shared/domain/entities/AbstractEntity.java
@@ -1,5 +1,19 @@
 package com.eduardoaf.balance.shared.domain.entities;
 
 public abstract class AbstractEntity {
-
+    public String processFlag;
+    public String insertPlatform;
+    public String insertUser;
+    public String insertDate;
+    public String updatePlatform;
+    public String updateUser;
+    public String updateDate;
+    public String deletePlatform;
+    public String deleteUser;
+    public String deleteDate;
+    public String cruCsvNote;
+    public String isErpSent;
+    public String isEnabled;
+    public Integer i;
+    public int id;
 }
