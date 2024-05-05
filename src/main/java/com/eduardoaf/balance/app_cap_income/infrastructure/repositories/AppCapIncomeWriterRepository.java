8c6342926ee359a3f4200acf32150945e1506878
ioedumac <eacevedof@yahoo.es>
2024-05-05 20:32:35 +0200

    AppCapIncomeWriterRepository

--- src/main/java/com/eduardoaf/balance/app_cap_income/infrastructure/repositories/AppCapIncomeWriterRepository.java
+++ src/main/java/com/eduardoaf/balance/app_cap_income/infrastructure/repositories/AppCapIncomeWriterRepository.java
@@ -1,13 +1,12 @@
 package com.eduardoaf.balance.app_cap_income.infrastructure.repositories;
 
-import com.eduardoaf.balance.app_cap_income.domain.entities.AppCapIncomeEntity;
-import com.eduardoaf.balance.shared.infrastructure.formatters.Uuid;
-import com.eduardoaf.balance.shared.infrastructure.repositories.AbstractMysqlRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.eduardoaf.balance.shared.infrastructure.formatters.Uuid;
 import com.eduardoaf.balance.shared.infrastructure.formatters.Date;
-
+import com.eduardoaf.balance.app_cap_income.domain.entities.AppCapIncomeEntity;
+import com.eduardoaf.balance.shared.infrastructure.repositories.AbstractMysqlRepository;
 import com.eduardoaf.balance.shared.infrastructure.db.query_builders.InsertQuery;
 
 @Component
