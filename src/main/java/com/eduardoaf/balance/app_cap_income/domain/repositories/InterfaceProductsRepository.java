dc155be526caa55f2d971ee67c063cf1404651cf
ioedumac <eacevedof@yahoo.es>
2024-05-11 18:52:29 +0200

    quito products-objects

--- src/main/java/com/eduardoaf/balance/app_cap_income/domain/repositories/InterfaceProductsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eduardoaf.balance.app_cap_income.domain.repositories;
-
-import com.eduardoaf.balance.app_cap_income.domain.entities.ProductEntity;
-import java.util.List;
-
-
-public interface InterfaceProductsRepository {
-    public List<ProductEntity> getAllProducts();
-}
