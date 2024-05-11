6676f3425cdfa1b7041352841a626d1d762121ef
ioedumac <eacevedof@yahoo.es>
2024-05-11 18:26:22 +0200

    validator

--- src/main/java/com/eduardoaf/balance/app_cap_income/infrastructure/controllers/CreateIncomeController.java
+++ src/main/java/com/eduardoaf/balance/app_cap_income/infrastructure/controllers/CreateIncomeController.java
@@ -1,5 +1,7 @@
 package com.eduardoaf.balance.app_cap_income.infrastructure.controllers;
 
+import com.eduardoaf.balance.app_cap_income.application.exceptions.CreateIncomeException;
+import com.eduardoaf.balance.app_cap_income.application.validators.CreateIncomeValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,16 +17,19 @@ import com.eduardoaf.balance.app_cap_income.application.services.CreateIncomeSer
 public final class CreateIncomeController {
 
     private final Log log;
+    private final CreateIncomeValidator createIncomeValidator;
     private final CreateIncomeService createIncomeService;
     private final HttpResponse httpResponse;
 
     @Autowired
     public CreateIncomeController(
         Log log,
+        CreateIncomeValidator createIncomeValidator,
         CreateIncomeService createIncomeService,
         HttpResponse httpResponse
     ) {
         this.log = log;
+        this.createIncomeValidator = createIncomeValidator;
         this.createIncomeService = createIncomeService;
         this.httpResponse = httpResponse;
     }
@@ -33,8 +38,11 @@ public final class CreateIncomeController {
     public ResponseEntity<?> invoke(@RequestBody CreateIncomeDto createIncomeDto) {
         try {
             var createdIncomeDto = createIncomeService.invoke(createIncomeDto);
-
+            createIncomeValidator.invoke(createIncomeDto);
             return httpResponse.getResponse200("entity created", createdIncomeDto);
+        }
+        catch (CreateIncomeException e) {
+            return httpResponse.getResponse(e.getStatusCode(), e.getMessage());
         }
         catch (Exception e) {
             log.exception(e);
--- src/main/java/com/eduardoaf/balance/shared/infrastructure/http/responses/HttpResponse.java
+++ src/main/java/com/eduardoaf/balance/shared/infrastructure/http/responses/HttpResponse.java
@@ -50,6 +50,36 @@ public final class  HttpResponse{
         return ResponseEntity.status(HttpStatus.OK).body(httpResponse);
     }
 
+    public ResponseEntity<?> getResponse(int statusCode, String message) {
+        var httpResponse = HttpResponse.getInstance(
+                statusCode,
+                Integer.toString(statusCode),
+                message,
+                null
+        );
+        return ResponseEntity.status(statusCode).body(httpResponse);
+    }
+
+    public ResponseEntity<?> getResponse(int statusCode, String message, Object data) {
+        var httpResponse = HttpResponse.getInstance(
+                statusCode,
+                Integer.toString(statusCode),
+                message,
+                data
+        );
+        return ResponseEntity.status(statusCode).body(httpResponse);
+    }
+
+    public ResponseEntity<?> getResponse400(String message) {
+        var httpResponse = HttpResponse.getInstance(
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.name(),
+                message,
+                null
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(httpResponse);
+    }
+
     public ResponseEntity<?> getResponse500(String message) {
         var httpResponse = HttpResponse.getInstance(
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
@@ -59,4 +89,5 @@ public final class  HttpResponse{
         );
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(httpResponse);
     }
+
 }
