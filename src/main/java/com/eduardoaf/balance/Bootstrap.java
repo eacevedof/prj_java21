4a0517a55f938d538ec525e62a30396cd0752a5a
ioedumac <eacevedof@yahoo.es>
2024-05-12 21:45:28 +0200

    ya levanta

--- src/main/java/com/eduardoaf/balance/Bootstrap.java
+++ src/main/java/com/eduardoaf/balance/Bootstrap.java
@@ -2,7 +2,10 @@ package com.eduardoaf.balance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import com.eduardoaf.balance.config.WebSecurityConfig;
 
+@Import(WebSecurityConfig.class)
 @SpringBootApplication
 public class Bootstrap {
 
