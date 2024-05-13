a951b78d6068c3b86a9fc002bab9f4113725f9fd
ioedumac <eacevedof@yahoo.es>
2024-05-13 23:02:49 +0200

    app-cacp-income

--- src/main/java/com/eduardoaf/balance/app_cap_income/domain/entities/AppCapIncomeEntity.java
+++ src/main/java/com/eduardoaf/balance/app_cap_income/domain/entities/AppCapIncomeEntity.java
@@ -1,12 +1,11 @@
 package com.eduardoaf.balance.app_cap_income.domain.entities;
 
 import com.eduardoaf.balance.shared.domain.entities.AbstractEntity;
-
-import java.util.Date;
+import com.eduardoaf.balance.shared.infrastructure.formatters.NumberFormatter;
+import com.eduardoaf.balance.shared.infrastructure.formatters.StringFormatter;
 
 public final class AppCapIncomeEntity extends AbstractEntity {
-
-    public final int id;
+    public final Integer id;
     public final String uuid;
     public final String codeErp;
     public final String description;
@@ -15,15 +14,25 @@ public final class AppCapIncomeEntity extends AbstractEntity {
     public final String incomeDate;
     public final double amount;
     public final String notes;
-    public final int idOwner;
+    public final Integer idOwner;
     public final String cruCsvNote;
-    public final int isErpSent;
-    public final int isEnabled;
+    public final Integer isErpSent;
+    public final Integer isEnabled;
 
     public AppCapIncomeEntity(
-        int id, String uuid, String codeErp, String description, String paymentFor, String payedFrom,
-        String incomeDate, double amount, String notes, int idOwner, String cruCsvNote, int isErpSent,
-        int isEnabled
+        Integer id, 
+        String uuid, 
+        String codeErp, 
+        String description, 
+        String paymentFor, 
+        String payedFrom,
+        String incomeDate, 
+        double amount, 
+        String notes, 
+        Integer idOwner, 
+        String cruCsvNote, 
+        Integer isErpSent,
+        Integer isEnabled
     ) {
         this.id = id;
         this.uuid = uuid;
@@ -41,23 +50,63 @@ public final class AppCapIncomeEntity extends AbstractEntity {
     }
 
     public static AppCapIncomeEntity getInstance(
-        int id, String uuid, String codeErp, String description, String paymentFor, String payedFrom,
-        String incomeDate, double amount, String notes, int idOwner, String cruCsvNote, int isErpSent,
-        int isEnabled
+        Integer id,
+        String uuid,
+        String codeErp,
+        String description,
+        String paymentFor,
+        String payedFrom,
+        String incomeDate,
+        double amount,
+        String notes,
+        Integer idOwner,
+        String cruCsvNote,
+        Integer isErpSent,
+        Integer isEnabled
     ) {
         return new AppCapIncomeEntity(
-            id, uuid,codeErp, description, paymentFor, payedFrom, incomeDate, amount,
-            notes, idOwner, cruCsvNote, isErpSent, isEnabled
+            id, 
+            uuid,
+            codeErp,
+            description, 
+            paymentFor,
+            payedFrom, 
+            incomeDate, 
+            amount,
+            notes, 
+            idOwner, 
+            cruCsvNote, 
+            isErpSent, 
+            isEnabled
         );
     }
 
     public static AppCapIncomeEntity getInstance(
-            String codeErp, String description, String paymentFor, String payedFrom,
-            String incomeDate, double amount, String notes, int idOwner
+        String codeErp, 
+        String description, 
+        String paymentFor, 
+        String payedFrom,
+        String incomeDate, 
+        double amount, 
+        String notes, 
+        Integer idOwner
     ) {
+        NumberFormatter numberFormatter = NumberFormatter.getInstance();
+        StringFormatter stringFormatter = StringFormatter.getInstance();
         return new AppCapIncomeEntity(
-                0, null, codeErp, description, paymentFor, payedFrom, incomeDate, amount,
-                notes, idOwner, null, 0, 1
+            numberFormatter.getNull(), 
+            stringFormatter.getNull(), 
+            codeErp, 
+            description, 
+            paymentFor, 
+            payedFrom, 
+            incomeDate, 
+            amount,
+            notes, 
+            idOwner, 
+            stringFormatter.getNull(), 
+            0, 
+            1
         );
     }
 }
