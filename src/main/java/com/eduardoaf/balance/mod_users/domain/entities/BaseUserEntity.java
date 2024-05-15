c567b3529d4219e6257d8b3a45485e5ba01df95b
ioedumac <eacevedof@yahoo.es>
2024-05-15 16:46:12 +0200

    backup

--- src/main/java/com/eduardoaf/balance/mod_users/domain/entities/BaseUserEntity.java
+++ src/main/java/com/eduardoaf/balance/mod_users/domain/entities/BaseUserEntity.java
@@ -9,26 +9,26 @@ import java.util.Map;
 import lombok.Getter;
 
 @Getter
-public final class BaseUserEntity extends AbstractEntity {
+public class BaseUserEntity extends AbstractEntity {
 
-    public final String uuid;
-    public final String codeErp;
-    public final String description;
-    public final String email;
-    public final String secret;
-    public final String phone;
-    public final String fullname;
-    public final String address;
-    public final String birthdate;
-    public final Integer idParent;
-    public final Integer idGender;
-    public final Integer idNationality;
-    public final Integer idCountry;
-    public final Integer idLanguage;
-    public final Integer idProfile;
-    public final String urlPicture;
-    public final String dateValidated;
-    public final Integer logAttempts;
+    private final String uuid;
+    private final String codeErp;
+    private final String description;
+    private final String email;
+    private final String secret;
+    private final String phone;
+    private final String fullname;
+    private final String address;
+    private final String birthdate;
+    private final Integer idParent;
+    private final Integer idGender;
+    private final Integer idNationality;
+    private final Integer idCountry;
+    private final Integer idLanguage;
+    private final Integer idProfile;
+    private final String urlPicture;
+    private final String dateValidated;
+    private final Integer logAttempts;
 
     public BaseUserEntity(
         Integer id,
@@ -103,54 +103,6 @@ public final class BaseUserEntity extends AbstractEntity {
         return baseUserEntity;
     }
 
-    public static BaseUserEntity getInstanceFromStrings(
-        String id,
-        String uuid,
-        String codeErp,
-        String description,
-        String email,
-        String secret,
-        String phone,
-        String fullname,
-        String address,
-        String birthdate,
-
-        String idParent,
-        String idGender,
-        String idNationality,
-        String idCountry,
-        String idLanguage,
-        String idProfile,
-
-        String urlPicture,
-        String dateValidated,
-        String logAttempts
-    ) {
-        StringFormatter stringFormatter = new StringFormatter();
-        NumberFormatter numberFormatter = new NumberFormatter();
-        
-        return new BaseUserEntity(
-            numberFormatter.getIntegerOrNull(id),
-            stringFormatter.getTrimOrNull(uuid),
-            stringFormatter.getTrimOrNull(codeErp),
-            stringFormatter.getTrimOrNull(description),
-            stringFormatter.getTrimOrNull(email),
-            stringFormatter.getTrimOrNull(secret),
-            stringFormatter.getTrimOrNull(phone),
-            stringFormatter.getTrimOrNull(fullname),
-            stringFormatter.getTrimOrNull(address),
-            stringFormatter.getTrimOrNull(birthdate),
-            numberFormatter.getIntegerOrNull(idParent),
-            numberFormatter.getIntegerOrNull(idGender),
-            numberFormatter.getIntegerOrNull(idNationality),
-            numberFormatter.getIntegerOrNull(idCountry),
-            numberFormatter.getIntegerOrNull(idLanguage),
-            numberFormatter.getIntegerOrNull(idProfile),
-            stringFormatter.getTrimOrNull(urlPicture),
-            stringFormatter.getTrimOrNull(dateValidated),
-            numberFormatter.getIntegerOrNull(logAttempts)
-        );
-    }
 
     public static BaseUserEntity getInstance(
             Integer id,
